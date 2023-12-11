fb21e989bdc7d34ac7bfe86e057175fe45a970c9
iskoto <ismail@yldrm.com.tr>
2023-12-11 04:29:54 +0300

    .

--- obj/Debug/net6.0-windows/toolsuiteapp.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/toolsuiteapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("toolsuiteapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddde1c4f4203506339cd261bd236bbe6d24db539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("toolsuiteapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("toolsuiteapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
